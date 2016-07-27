e6b69476ab4f82d5af724ddb0a03ef300f83f076
andygulin <andygulin@hotmail.com>
2016-07-27 13:28:18 +0800

    ...

--- dubbo-sample-consumer/src/test/java/dubbo/sample/consumer/Consumer.java
+++ dubbo-sample-consumer/src/test/java/dubbo/sample/consumer/Consumer.java
@@ -1,11 +1,14 @@
 package dubbo.sample.consumer;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+import com.alibaba.dubbo.common.json.JSON;
 
 import dubbo.sample.bean.User;
 import dubbo.sample.service.HelloService;
@@ -21,14 +24,19 @@ public class Consumer extends AbstractJUnit4SpringContextTests {
 
 	@Test
 	public void hello() {
-		System.out.println(helloService.hello("andy"));
+		String str = helloService.hello("andy");
+		System.out.println("HelloService#hello ---------- " + str);
 	}
 
 	@Test
-	public void user() {
+	public void user() throws IOException {
 		User user = userService.getUser(1);
-		System.out.println(user);
+		System.out.println("HelloService#getUser(1) ---------- " + JSON.json(user));
+	}
+
+	@Test
+	public void list() throws IOException {
 		List<User> users = userService.getUserList();
-		System.out.println(users);
+		System.out.println("HelloService#getUserList ---------- " + JSON.json(users));
 	}
 }
